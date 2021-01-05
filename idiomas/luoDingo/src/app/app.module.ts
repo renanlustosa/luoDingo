85364a081a51a4b094c18967d520bc2d9895910e
Renan Lustosa <renanferreiralustosa@gmail.com>
2021-01-05 15:17:51 -0300

    bootstrap instalado

--- idiomas/luoDingo/src/app/app.module.ts
+++ idiomas/luoDingo/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+import { PainelComponent } from './painel/painel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    TentativasComponent,
+    ProgressoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
