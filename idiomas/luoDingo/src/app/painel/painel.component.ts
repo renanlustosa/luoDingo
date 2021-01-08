27f799cd8cbea54f661577275fd1902772d43109
Renan Lustosa <renanferreiralustosa@gmail.com>
2021-01-08 18:30:05 -0300

    atualizaResposta e verificaResposta implementados

--- idiomas/luoDingo/src/app/painel/painel.component.ts
+++ idiomas/luoDingo/src/app/painel/painel.component.ts
@@ -12,11 +12,33 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Escreva em português:'
+  public resposta: string = ''
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
 
 
-  constructor() { }
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+
+  }
 
   ngOnInit(): void {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+
+  }
+  public verificarResposta(): void {
+    
+    if(this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('CORRETO!')
+      this.rodada++
+      console.log(this.rodada)
+      this.rodadaFrase = this.frases[this.rodada]
+    } else {
+      alert('INCORRETO!')
+    }
+  }
 }
